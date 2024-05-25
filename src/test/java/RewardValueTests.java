dd47500c1abc64f08cefd09365241af35bf4bf22
normajean8 <154831617+normajean8@users.noreply.github.com>
2024-05-25 12:14:16 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,31 +1,30 @@
-import org.junit.jupiter.api.Test;
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+    private static final double MILES_TO_CASH_RATE = 0.0035;
 
-public class RewardValueTests {
-
-    @Test
-    void create_with_cash_value() {
-        double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+    // Constructor accepting cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int)(cashValue / MILES_TO_CASH_RATE);
     }
 
-    @Test
-    void create_with_miles_value() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+    // Constructor accepting miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_RATE;
     }
 
-    @Test
-    void convert_from_cash_to_miles() {
-        assert false;
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
     }
 
-    @Test
-    void convert_from_miles_to_cash() {
-        assert false;
+    // Method to get the miles value
+    public int getMilesValue() {
+        return milesValue;
     }
 }
+
 
